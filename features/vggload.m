d0cf631f1fc46a68b3f4e57dbeaaee18b4daf3ad
Abhilash_Srikantha <asrikantha@informatik.uni-bonn.de>
2015-11-09 17:38:33 +0100

    bugfix features/vggload()

--- features/vggload.m
+++ features/vggload.m
@@ -1,7 +1,12 @@
 function out=vggload(path,tag) % assumes first level is 2_2 (=240*320, half resolution == resolution for training)
     out = [];
-    for lev = 1:numel(tag)
-        feat=load(sprintf(path,tag{lev}));
+    for idx = 1:numel(tag)
+		if(strcmp(tag{idx},'2_2'))	lev=1;
+		elseif(strcmp(tag{idx},'3_4')) lev=2;
+		elseif(strcmp(tag{idx},'4_4')) lev=3;
+		elseif(strcmp(tag{idx},'5_4')) lev=4;
+		end
+        feat=load(sprintf(path,tag{idx}));
         feat=feat.scores;
         feat=imresize(feat,2^lev,'nearest');
         out = cat(3,out,feat);
